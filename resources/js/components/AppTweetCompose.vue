b6db160545e272c4cce771c47b06dac6a38e7db4
Nguyên Vũ <vuducnguyentb@gmail.com>
2024-03-12 11:09:11 +0700

    update

--- resources/js/components/AppTweetCompose.vue
+++ resources/js/components/AppTweetCompose.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div class="flex-grow">
-            {{form}}
+            {{ form }}
             <textarea class="bg-gray-600 w-full outline-none text-gray-300
 text-lg resize-none mb-2"
                       autofocus
@@ -30,17 +30,17 @@ px-4 font-bold leading-none
 import axios from "axios";
 
 export default {
-    data(){
+    data() {
         return {
-            form:{
-                body:''
+            form: {
+                body: ''
             }
         }
     },
-    methods:{
-        async submit()
-        {
-            await axios.post('/api/tweets',this.form)
+    methods: {
+        async submit() {
+            await axios.post('/api/tweets', this.form)
+            this.form.body = ''
         }
     }
 }
