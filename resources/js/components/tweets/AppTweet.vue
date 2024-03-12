116457300c0120b5b66dce055becf07f35e4102e
Nguyên Vũ <vuducnguyentb@gmail.com>
2024-03-12 11:06:43 +0700

    update

--- resources/js/components/tweets/AppTweet.vue
+++ resources/js/components/tweets/AppTweet.vue
@@ -4,19 +4,6 @@
         :is="`app-tweet-variant-${tweet.type}`"
         :tweet = "tweet"
         />
-
-
-        <!--        <div class="flex w-full">-->
-        <!--            <div class="mr-3">-->
-        <!--                <img :src="tweet.user.avatar" alt="" class="w-12 rounded-12">-->
-        <!--            </div>-->
-        <!--        </div>-->
-        <!--        <app-tweet-username-->
-        <!--            :user="tweet.user"-->
-        <!--        />-->
-        <!--        <p class="text-gray-300 whitespace-pre-wrap">-->
-        <!--            {{ tweet.body }}-->
-        <!--        </p>-->
     </div>
 </template>
 <script>
--- resources/js/components/tweets/Variants/AppTweetVariantQuote.vue
+++ resources/js/components/tweets/Variants/AppTweetVariantQuote.vue
@@ -5,22 +5,26 @@
         </div>
     </div>
     <div class="flex-grow">
-
         <app-tweet-username
             :user="tweet.user"
         />
         <p class="text-gray-300 whitespace-pre-wrap">
             {{ tweet.body }}
         </p>
-<!--        <app-tweet-->
-<!--            class="border border-gray-700 rounded-lg mt-4"-->
-<!--            :tweet="tweet.original_tweet"-->
-<!--        />-->
+
+        <app-tweet
+        class="border border-gray-700 rounded-lg mt-4"
+        :tweet="tweet.original_tweet"
+        />
+
     </div>
 </template>
 
 <script>
+import AppTweet from "@/components/tweets/AppTweet.vue";
+
 export default {
+    components: {AppTweet},
     props: {
         tweet: {
             required: true,
