9ea159612934480fa6b2a597c7e64c6bbcb0601c
Nguyên Vũ <vuducnguyentb@gmail.com>
2024-03-14 13:44:04 +0700

    update

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -4,19 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Tweet extends Model
 {
     use HasFactory;
 
-    protected  $guarded = [];
+    protected $guarded = [];
 
-    public function user(){
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function originalTweet()
+    public function originalTweet(): HasOne
     {
-        return $this->hasOne(Tweet::class,'id','original_tweet_id');
+        return $this->hasOne(Tweet::class, 'id', 'original_tweet_id');
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
     }
 }
